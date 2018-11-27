997024b5e24e1d7f3f92c439f706b8ef0c0f04f8
hiranokoto <udalov.91@mail.ru>
2018-11-27 20:35:46 +0500

    Сумма нечётных положительных чисел

---
+++ Lesson 2/Подсчитать сумму всех нечётных положительных чисел (Удалов)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Подсчитать_сумму_всех_нечётных_положительных_чисел__Удалов_
+{
+    class Program
+    {
+        static bool Odd(int a)
+        {
+            return a % 2 == 0;
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Введите целое число.");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x + " - " + ((Odd(x)) ? "четное число" : "нечетное число"));
+            Console.WriteLine("Для выхода из приложение нажмите Enter");
+            Console.ReadLine();
+        }
+
+    }
+}
