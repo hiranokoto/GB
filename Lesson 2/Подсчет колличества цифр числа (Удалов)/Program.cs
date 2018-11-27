b5d88c5417fda4c081a0ee1f995240037841b482
hiranokoto <udalov.91@mail.ru>
2018-11-27 20:34:52 +0500

    Подсчет колличества цифр числа

---
+++ Lesson 2/Подсчет колличества цифр числа (Удалов)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Подсчет_колличества_цифр_числа__Удалов_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Число: ");
+            Console.WriteLine("Количество знаков: " + count(Console.ReadLine()));
+            Console.ReadKey();
+        }
+
+        static int count(string s)
+        {
+            return s.Length;
+        }
+    }
+    
+}
