8e162b22182514a820bae4434fc5bf2f99293f69
hiranokoto <udalov.91@mail.ru>
2018-11-27 20:34:06 +0500

    Минимальное из трёх чисел.

---
+++ Lesson 2/Lesson 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, min;
+            Console.WriteLine("Введите три целых числа.");
+            Console.Write("Первое число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Второе число:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Третье число");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+               min = a;
+            }
+            else 
+            {
+                min = b;
+            }
+            if (a < c)
+            {
+                min = a;
+            }
+            else 
+            {
+                min = c;
+            }
+            if (b < c)
+            {
+                min = b;
+            }
+            else
+            {
+                min = c;
+            };
+            Console.WriteLine("Наименьшее число {0}", min);
+            Console.WriteLine("Для выхода из приложение нажмите Enter");
+            Console.ReadLine();
+
+        }
+    }
+}
